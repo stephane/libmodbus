cc7591bb817c28a0a921609fda0115b8651403eb
Stéphane Raimbault <stephane.raimbault@gmail.com>
2008-02-28 09:03:08 +0100

    License changes - modbus.[ch] are LGPL - test-modbus-master.c and test-modbus-slave.c are GPL

--- src/modbus.c
+++ src/modbus.c
@@ -1,24 +1,30 @@
 /* 
-   Copyright (C) 2001-2007 Stéphane Raimbault <stephane.raimbault@gmail.com>
-
+   Copyright (C) 2001-2008 Stéphane Raimbault <stephane.raimbault@gmail.com>
+  
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+  
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+  
    You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+   License along with this library; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
-   The functions included here have been derived from the Modicon
-   Modbus Protocol Reference Guide which can be obtained from
-   Schneider at www.schneiderautomation.com.    
-   
+/*
+   The library is designed to send and receive data from a device that
+   communicate via the Modbus protocol.
+
+   The function names used are inspired by the Modicon Modbus Protocol
+   Reference Guide which can be obtained from Schneider at
+   www.schneiderautomation.com.
+    
    Documentation:
    http://www.easysw.com/~mike/serial/serial.html
 */
--- src/test-modbus-master.c
+++ src/test-modbus-master.c
@@ -1,23 +1,21 @@
 /*
-   Copyright (C) 2001-2007 Stéphane Raimbault <stephane.raimbault@gmail.com>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-   
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
-
-   These library of functions are designed to enable a program send and
-   receive data from a device that communicates using the Modbus protocol.
-*/
+ * Copyright (C) 2001-2008 Stéphane Raimbault <stephane.raimbault@gmail.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <unistd.h>
