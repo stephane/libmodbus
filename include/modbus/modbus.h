68b5f998af37d5950210b66b24324e06ea42ab32
Stéphane Raimbault <stephane.raimbault@gmail.com>
2008-02-28 09:03:08 +0100

    License changes - modbus.[ch] are LGPL - test-modbus-master.c and test-modbus-slave.c are GPL

--- include/modbus/modbus.h
+++ include/modbus/modbus.h
@@ -1,22 +1,20 @@
 /* 
-   Copyright (C) 2001-2007 Stéphane Raimbault <stephane.raimbault@gmail.com>
-
+   Copyright (C) 2001-2008 Stéphane Raimbault <stephane.raimbault@gmail.com>
+  
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+  
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+  
    You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
-
-   These library of functions are designed to enable a program send and
-   receive data from a device that communicates using the Modbus protocol.
+   License along with this library; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
 */
 
 #ifndef _MODBUS_H_
