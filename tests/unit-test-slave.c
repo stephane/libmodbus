a017c19f2a251b869028d5e2999c97f3055583c2
Stéphane Raimbault <stephane.raimbault@gmail.com>
2008-05-02 00:54:52 +0200

    GNU licenses updated to the version 3.

--- tests/unit-test-slave.c
+++ tests/unit-test-slave.c
@@ -1,11 +1,9 @@
 /*
- * Copyright (C) 2001-2008 Stéphane Raimbault <stephane.raimbault@gmail.com>
+ * Copyright © 2008 Stéphane Raimbault <stephane.raimbault@gmail.com>
  *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
