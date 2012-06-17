50519d449ce76d78ff17f7e3f36b87be9760a275
Stéphane Raimbault <stephane.raimbault@gmail.com>
2012-06-17 15:37:35 +0200

    Remove useless test on error_recovery argument
    Thanks to Carlos Tangerino

--- src/modbus.c
+++ src/modbus.c
@@ -1402,13 +1402,8 @@ int modbus_set_slave(modbus_t *ctx, int slave)
 int modbus_set_error_recovery(modbus_t *ctx,
                               modbus_error_recovery_mode error_recovery)
 {
-    if (error_recovery >= 0) {
-        ctx->error_recovery = (uint8_t) error_recovery;
-    } else {
-        errno = EINVAL;
-        return -1;
-    }
-
+    /* The type of modbus_error_recovery_mode is unsigned enum */
+    ctx->error_recovery = (uint8_t) error_recovery;
     return 0;
 }
 
