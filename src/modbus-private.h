44fb25b5dba36c0277c7f4231313f1658792f63c
Woodrow Douglass <wdouglass@carnegierobotics.com>
2013-05-22 09:55:55 -0400

    Made function codes public, for use in application callbacks

--- src/modbus-private.h
+++ src/modbus-private.h
@@ -51,20 +51,6 @@ MODBUS_BEGIN_DECLS
 #define _RESPONSE_TIMEOUT    500000
 #define _BYTE_TIMEOUT        500000
 
-/* Function codes */
-#define _FC_READ_COILS                0x01
-#define _FC_READ_DISCRETE_INPUTS      0x02
-#define _FC_READ_HOLDING_REGISTERS    0x03
-#define _FC_READ_INPUT_REGISTERS      0x04
-#define _FC_WRITE_SINGLE_COIL         0x05
-#define _FC_WRITE_SINGLE_REGISTER     0x06
-#define _FC_READ_EXCEPTION_STATUS     0x07
-#define _FC_WRITE_MULTIPLE_COILS      0x0F
-#define _FC_WRITE_MULTIPLE_REGISTERS  0x10
-#define _FC_REPORT_SLAVE_ID           0x11
-#define _FC_MASK_WRITE_REGISTER       0x16
-#define _FC_WRITE_AND_READ_REGISTERS  0x17
-
 typedef enum {
     _MODBUS_BACKEND_TYPE_RTU=0,
     _MODBUS_BACKEND_TYPE_TCP
