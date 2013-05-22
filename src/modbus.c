1fa1a478dd4d5c340a3528f964b09d03753b2ef0
Woodrow Douglass <git@wdouglass.com>
2013-05-22 10:41:16 -0400

    Remove some '-Wshadow' warnings

--- src/modbus.c
+++ src/modbus.c
@@ -1938,13 +1938,13 @@ static int modbus_read_any_bits(modbus_t *ctx, uint16_t address, int nb,
 int modbus_response_set_register(uint8_t *rsp_buf, int *size,
 	uint16_t base_address, int address, uint16_t value)
 {
-    int index = 2 * (address - base_address);
+    int idx = 2 * (address - base_address);
 
-    if (index+1 >= *size)
+    if (idx+1 >= *size)
       return MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS;
 
-    rsp_buf[index++] = value >> 8;
-    rsp_buf[index] = value & 0xFF;
+    rsp_buf[idx++] = value >> 8;
+    rsp_buf[idx] = value & 0xFF;
 
     return 0;
 }
@@ -1952,12 +1952,12 @@ int modbus_response_set_register(uint8_t *rsp_buf, int *size,
 int modbus_request_get_register(uint8_t *req_buf, int size,
 	uint16_t base_address, int address, uint16_t *value)
 {
-    int index = 2 * (address - base_address);
+    int idx = 2 * (address - base_address);
 
-    if (index+1 >= size)
+    if (idx+1 >= size)
       return MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS;
 
-    *value = (req_buf[index] << 8) + req_buf[index+1];
+    *value = (req_buf[idx] << 8) + req_buf[idx+1];
 
     return 0;
 }
@@ -1965,19 +1965,19 @@ int modbus_request_get_register(uint8_t *req_buf, int size,
 int modbus_response_set_bit(uint8_t *rsp_buf, int *size,
 	uint16_t base_address, int address, uint16_t value)
 {
-    int index = (address - base_address) / 8;
+    int idx = (address - base_address) / 8;
     int shift = (address - base_address) % 8;
     uint8_t bit;
 
-    if (index >= *size)
+    if (idx >= *size)
       return MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS;
 
     bit = 0x01 << shift;
 
     if (value)
-	rsp_buf[index] |= bit;
+	rsp_buf[idx] |= bit;
     else
-	rsp_buf[index] &=~ bit;
+	rsp_buf[idx] &=~ bit;
 
     return 0;
 }
@@ -1985,16 +1985,16 @@ int modbus_response_set_bit(uint8_t *rsp_buf, int *size,
 int modbus_request_get_bit(uint8_t *req_buf, int size,
 	uint16_t base_address, int address, uint16_t *value)
 {
-    int index = (address - base_address) / 8;
+    int idx = (address - base_address) / 8;
     int shift = (address - base_address) % 8;
     uint8_t bit;
 
-    if (index >= size)
+    if (idx >= size)
       return MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS;
 
     bit = 0x01 << shift;
 
-    *value = (req_buf[index] & bit) == bit;
+    *value = (req_buf[idx] & bit) == bit;
 
     return 0;
 }
