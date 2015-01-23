d9aa3d19fc86deb6e382ff3b80db1c2d0b33be07
Michael Heimpold <mhei@heimpold.de>
2015-01-24 00:43:52 +0100

    Improve unit-tests.sh
    - fix bash'isms
    - simplify
    - terminate server after test run
    
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -1,32 +1,31 @@
-#!/bin/bash
-
-client=$( find -maxdepth 1 -executable -type f -iname '*unit-test-client*')
-if [ "$client" == "" ]; then
-    echo "Unit-test-client is not found"
-    return -1;
-fi
-echo "Client executable found"
-
-server=$( find -maxdepth 1 -executable -type f -iname '*unit-test-server*')
-if [ "$client" == "" ]; then
-    echo "Unit-test-server is not found"
-    return -1;
-fi
-echo "Server executable found"
-
+#!/bin/sh
 
 client_log=unit-test-client.log
 server_log=unit-test-server.log
 
-if [ -f $client_log ]; then rm $client_log; fi;
-if [ -f $server_log ]; then rm $server_log; fi;
+client=$(find -maxdepth 1 -executable -type f -iname '*unit-test-client*')
+if [ -z "$client" ]; then
+    echo "Unit-test-client is not found"
+    exit 1
+fi
+echo "Client executable found"
+
+server=$(find -maxdepth 1 -executable -type f -iname '*unit-test-server*')
+if [ -z "$server" ]; then
+    echo "Unit-test-server is not found"
+    exit 1
+fi
+echo "Server executable found"
+
+rm -f $client_log $server_log
 
 echo "Starting server"
-$server 2>&1 > $server_log &
+$server > $server_log 2>&1 &
+server_pid=$!
 sleep 1
 echo "Server started"
 
 echo "Starting client"
 $client | tee $client_log
 
-
+kill $server_pid
