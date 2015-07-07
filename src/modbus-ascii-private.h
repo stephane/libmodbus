64ed817412a9174e83f71ebe09c905f620bbe776
Jakob Bysewski <jakob@bysewski.de>
2015-07-07 11:18:25 +0200

    Created modbus-ascii

---
+++ src/modbus-ascii-private.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2001-2011 Stéphane Raimbault <stephane.raimbault@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#ifndef MODBUS_ASCII_PRIVATE_H
+#define MODBUS_ASCII_PRIVATE_H
+
+#define _MODBUS_ASCII_HEADER_LENGTH      2
+#define _MODBUS_ASCII_PRESET_REQ_LENGTH  7
+#define _MODBUS_ASCII_PRESET_RSP_LENGTH  2
+
+#define _MODBUS_ASCII_CHECKSUM_LENGTH    3 /* lrc8 + \r\n */
+
+#endif /* MODBUS_ASCII_PRIVATE_H */
