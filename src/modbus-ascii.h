42e70d1aaaef7cffa429676779272f839272cc31
Stéphane Raimbault <stephane.raimbault@gmail.com>
2016-05-17 16:35:34 +0200

    Fix wrong max ADU length of ASCII backend

--- src/modbus-ascii.h
+++ src/modbus-ascii.h
@@ -13,10 +13,10 @@ MODBUS_BEGIN_DECLS
 
 /* Modbus_over_serial_line_V1_02.pdf - 2.5.2.1 MODBUS Message ASCII Framing
  * Page 17
- * RS232 / RS485 ADU =
+ * RS232/RS485 ADU =
  * start (1 byte) + address (2 bytes) + function (2 bytes) + 2x252 bytes + LRC (2 bytes) + end (2 bytes) = 513 bytes
  */
-#define MODBUS_ASCII_MAX_ADU_LENGTH  256
+#define MODBUS_ASCII_MAX_ADU_LENGTH 513
 
 MODBUS_API modbus_t* modbus_new_ascii(const char *device, int baud, char parity,
                                       int data_bit, int stop_bit);
