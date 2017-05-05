7a6078b6bcd952a53311bd14610782f754f194ef
Stéphane Raimbault <stephane.raimbault@gmail.com>
2017-05-05 16:06:15 +0200

    Remove CYGWIN condition to provide bswap16 fallback (#383)

--- src/modbus-data.c
+++ src/modbus-data.c
@@ -54,7 +54,7 @@
 #  define bswap_16 _byteswap_ushort
 #endif
 
-#if !defined(__CYGWIN__) && !defined(bswap_16)
+#if !defined(bswap_16)
 #  warning "Fallback on C functions for bswap_16"
 static inline uint16_t bswap_16(uint16_t x)
 {
