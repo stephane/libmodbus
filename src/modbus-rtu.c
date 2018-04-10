d58c50f177f71e9e8a410d8f2ada4bffd039c059
Michael Heimpold <mhei@heimpold.de>
2018-04-11 00:27:44 +0200

    Add support for local echo suppression
    Usually, RS-485 hardware for half-duplex connections is designed to
    suppress self-generated TX traffic on the RX side. This is usually
    achived by controlling the Receive Enable pin of the driver with
    the same signal which toggles the Transmit Enable pin (traditionally
    one pin uses an inverted signal, so this is really easy to implement).
    
    However, some boards are designed differently in that they enable RX side
    unconditionally or make this configurable with e.g. a jumper.
    In this case you'll see all of your outgoing traffic also on
    incoming side. So to filter out this expected but unwanted traffic
    we could simply flush the incoming queue. But this way, we could
    drop erroneously bytes of the legitimate response from a peer, e.g.
    if the peer answers very quickly.
    
    The approach implemented here makes use of the knowledge how many bytes
    are expected and thus must be dropped. Do it directly in the transmitting
    function to prevent handing over the expected byte count through
    modbus context.
    
    Also add required functions to enable/disable this workaround and
    provide API documentation.
    
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- src/modbus-rtu.c
+++ src/modbus-rtu.c
@@ -272,32 +272,84 @@ static ssize_t _modbus_rtu_send(modbus_t *ctx, const uint8_t *req, int req_lengt
 #if defined(_WIN32)
     modbus_rtu_t *ctx_rtu = ctx->backend_data;
     DWORD n_bytes = 0;
-    return (WriteFile(ctx_rtu->w_ser.fd, req, req_length, &n_bytes, NULL)) ? (ssize_t)n_bytes : -1;
-#else
-#if HAVE_DECL_TIOCM_RTS
-    modbus_rtu_t *ctx_rtu = ctx->backend_data;
-    if (ctx_rtu->rts != MODBUS_RTU_RTS_NONE) {
-        ssize_t size;
+    ssize_t rv;
 
+    rv = WriteFile(ctx_rtu->w_ser.fd, req, req_length, &n_bytes, NULL) ? (ssize_t)n_bytes : -1;
+
+    /* if local echo workaround is enabled, try to discard the count of bytes
+     *  we sent ourself */
+    if (ctx_rtu->is_echo_suppressing && n_bytes > 0) {
+        /* we don't use a timeout here since we should have our own caused stuff returned
+         * immediately - and we know the internals of win32_ser_select from code above */
+        if (win32_ser_select(&ctx_rtu->w_ser, (int)n_bytes, NULL) <= 0) {
+             /* propagate errors and timeouts */
+             return -1;
+        } else {
+             /* discard the count of bytes we caused ourself, but keep possible bytes
+              * of following packet we already received */
+             unsigned int n = ctx_rtu->w_ser.n_bytes;
+
+             if (n > (unsigned int)n_bytes) {
+                 n = (unsigned int)n_bytes;
+             }
+
+             ctx_rtu->w_ser.n_bytes -= n;
+        }
+    }
+    return rv;
+#else
+    modbus_rtu_t *ctx_rtu = ctx->backend_data;
+    ssize_t write_size;
+
+#if HAVE_DECL_TIOCM_RTS
+    if (ctx_rtu->rts != MODBUS_RTU_RTS_NONE) {
         if (ctx->debug) {
             fprintf(stderr, "Sending request using RTS signal\n");
         }
 
         ctx_rtu->set_rts(ctx, ctx_rtu->rts == MODBUS_RTU_RTS_UP);
         usleep(ctx_rtu->rts_delay);
-
-        size = write(ctx->s, req, req_length);
-
-        usleep(ctx_rtu->onebyte_time * req_length + ctx_rtu->rts_delay);
-        ctx_rtu->set_rts(ctx, ctx_rtu->rts != MODBUS_RTU_RTS_UP);
-
-        return size;
-    } else {
-#endif
-        return write(ctx->s, req, req_length);
-#if HAVE_DECL_TIOCM_RTS
     }
 #endif
+
+    write_size = write(ctx->s, req, req_length);
+
+#if HAVE_DECL_TIOCM_RTS
+    if (ctx_rtu->rts != MODBUS_RTU_RTS_NONE) {
+        usleep(ctx_rtu->onebyte_time * req_length + ctx_rtu->rts_delay);
+        ctx_rtu->set_rts(ctx, ctx_rtu->rts != MODBUS_RTU_RTS_UP);
+    }
+#endif
+
+    /* if local echo workaround is enabled, try to discard the count of bytes
+     * we sent ourself */
+    if (ctx_rtu->is_echo_suppressing && write_size > 0) {
+        uint8_t req_echo[MODBUS_RTU_MAX_ADU_LENGTH];
+        ssize_t read_size = 0;
+
+        while (read_size < write_size) {
+            ssize_t count = read(ctx->s, &req_echo[read_size], write_size - read_size);
+
+            /* return immediately on error */
+            if (count < 0) {
+                return -1;
+            }
+
+            /* 'c' can also be zero - which usually indicates EOF - but this means
+             * in our case here that the echo-ed bytes are not yet available for reading
+             * back. We do busy waiting here to dispatch them as fast as possible. */
+            read_size += count;
+        }
+
+        /* compare packet sent out and echo read back: if both differ we know that
+         * the packet was stumbled and receiver will probably ignore it */
+        if (memcmp(req, req_echo, write_size) != 0) {
+            errno = EIO;
+            return -1;
+        }
+    }
+
+    return write_size;
 #endif
 }
 
@@ -1112,6 +1164,39 @@ int modbus_rtu_set_rts_delay(modbus_t *ctx, int us)
     }
 }
 
+int modbus_rtu_set_suppress_echo(modbus_t *ctx, bool on)
+{
+    if (ctx == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (ctx->backend->backend_type == _MODBUS_BACKEND_TYPE_RTU) {
+        modbus_rtu_t *ctx_rtu = ctx->backend_data;
+        ctx_rtu->is_echo_suppressing = on;
+        return 0;
+    } else {
+        errno = EINVAL;
+        return -1;
+    }
+}
+
+int modbus_rtu_get_suppress_echo(modbus_t *ctx)
+{
+    if (ctx == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (ctx->backend->backend_type == _MODBUS_BACKEND_TYPE_RTU) {
+        modbus_rtu_t *ctx_rtu;
+        ctx_rtu = (modbus_rtu_t *)ctx->backend_data;
+        return ctx_rtu->is_echo_suppressing;
+    } else {
+        return FALSE;
+    }
+}
+
 static void _modbus_rtu_close(modbus_t *ctx)
 {
     /* Restore line settings and close file descriptor in RTU mode */
@@ -1294,6 +1379,7 @@ modbus_t* modbus_new_rtu(const char *device,
 #endif
 
     ctx_rtu->confirmation_to_ignore = FALSE;
+    ctx_rtu->is_echo_suppressing = FALSE;
 
     return ctx;
 }
