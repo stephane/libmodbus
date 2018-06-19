d89f16e7de750f852907839f3f6bf841a7bcd99c
Michael Heimpold <mhei@heimpold.de>
2018-06-19 22:19:40 +0200

    modbus_read_input_registers: also guard debug output by context debug flag
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- src/modbus.c
+++ src/modbus.c
@@ -1211,9 +1211,11 @@ int modbus_read_input_registers(modbus_t *ctx, int addr, int nb,
     }
 
     if (nb > MODBUS_MAX_READ_REGISTERS) {
-        fprintf(stderr,
-                "ERROR Too many input registers requested (%d > %d)\n",
-                nb, MODBUS_MAX_READ_REGISTERS);
+        if (ctx->debug) {
+            fprintf(stderr,
+                    "ERROR Too many input registers requested (%d > %d)\n",
+                    nb, MODBUS_MAX_READ_REGISTERS);
+        }
         errno = EMBMDATA;
         return -1;
     }
