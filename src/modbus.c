5a7197a79603a623e9f5edf78d889bf9425fb4c3
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-03-31 23:43:36 +0100

    Enable compiler checking of switch(enum)
    Enable, as an error, compiler checking that a switch of a enum has all
    possible values covered. This also fixes the single case GCC detected.

--- src/modbus.c
+++ src/modbus.c
@@ -455,7 +455,7 @@ int _modbus_receive_msg(modbus_t *ctx, uint8_t *msg, msg_type_t msg_type)
                 }
                 step = _STEP_DATA;
                 break;
-            default:
+            case _STEP_DATA:
                 break;
             }
         }
