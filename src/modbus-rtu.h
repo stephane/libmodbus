8868b5ad874f7b9594f1d4a7fa4412f1d4afeb6d
epsilonrt <epsilonrt@gmail.com>
2020-02-27 19:18:23 +0100

    Add disabling flag for RTU address filtering
    Addition of the modbus_rtu_set_recv_filter() and modbus_rtu_get_recv_filter() functions.
    This makes it possible to process on reception all messages in RTU mode.
    
    Signed-off-by: epsilonrt <epsilonrt@gmail.com>

--- src/modbus-rtu.h
+++ src/modbus-rtu.h
@@ -37,6 +37,9 @@ MODBUS_API int modbus_rtu_set_custom_rts(modbus_t *ctx, void (*set_rts) (modbus_
 MODBUS_API int modbus_rtu_set_rts_delay(modbus_t *ctx, int us);
 MODBUS_API int modbus_rtu_get_rts_delay(modbus_t *ctx);
 
+MODBUS_API int modbus_rtu_set_recv_filter(modbus_t *ctx, int on);
+MODBUS_API int modbus_rtu_get_recv_filter(modbus_t *ctx);
+
 MODBUS_END_DECLS
 
 #endif /* MODBUS_RTU_H */
