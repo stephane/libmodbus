f2ea473adca1a5a833c9a2c1e1055f36ebe48488
Jim Klimov <jimklimov+nut@gmail.com>
2025-01-18 17:42:12 +0100

    src/modbus.c: response_exception(): fflush() before and after vfprintf() to sync the output
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- src/modbus.c
+++ src/modbus.c
@@ -768,7 +768,9 @@ static int response_exception(modbus_t *ctx,
         va_list ap;
 
         va_start(ap, template);
+        fflush(stderr);
         vfprintf(stderr, template, ap);
+        fflush(stderr);
         va_end(ap);
     }
 
