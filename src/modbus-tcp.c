63c6f2d063a9a7ff4c1f9b52b4b62873f06125dd
Jim Klimov <jimklimov@gmail.com>
2025-01-18 10:45:32 +0100

    configure.ac, src/modbus-tcp.c: import fallback inet_pton() and inet_ntop() from NUT for cross-builds on WIN32 (mingw)
    Adapted to libmodbus codebase and clang-format
    
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- src/modbus-tcp.c
+++ src/modbus-tcp.c
@@ -68,6 +68,217 @@
 #include "modbus-tcp.h"
 
 #ifdef OS_WIN32
+
+// inet_ntop() and inet_pton() fallbacks picked up from NUT v2.8.2 common/wincompat.c:
+#if !HAVE_INET_NTOP
+static const char *inet_ntop(int af, const void *src, char *dst, size_t cnt)
+{
+    /* Instead of WSAAddressToString() consider getnameinfo() if this would in fact
+     * return decorated addresses (brackets, ports...) as discussed below:
+     * https://users.ipv6.narkive.com/RXpR5aML/windows-and-inet-ntop-vs-wsaaddresstostring
+     * https://docs.microsoft.com/en-us/windows/win32/api/ws2tcpip/nf-ws2tcpip-getnameinfo
+     * https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-wsaaddresstostringa
+     */
+    switch (af) {
+    case AF_INET: {
+        struct sockaddr_in srcaddr;
+        memset(&srcaddr, 0, sizeof(struct sockaddr_in));
+        memcpy(&(srcaddr.sin_addr), src, sizeof(srcaddr.sin_addr));
+        srcaddr.sin_family = af;
+        if (WSAAddressToString((struct sockaddr *) &srcaddr,
+                               sizeof(struct sockaddr_in),
+                               0,
+                               dst,
+                               (LPDWORD) &cnt) != 0) {
+            WSAGetLastError();
+            return NULL;
+        }
+    } break;
+
+    case AF_INET6:
+        /* NOTE: Since WinXP SP1, with IPv6 installed on the system */
+        {
+            struct sockaddr_in6 srcaddr;
+            memset(&srcaddr, 0, sizeof(struct sockaddr_in6));
+            memcpy(&(srcaddr.sin6_addr), src, sizeof(srcaddr.sin6_addr));
+            srcaddr.sin6_family = af;
+            if (WSAAddressToString((struct sockaddr *) &srcaddr,
+                                   sizeof(struct sockaddr_in6),
+                                   0,
+                                   dst,
+                                   (LPDWORD) &cnt) != 0) {
+                WSAGetLastError();
+                return NULL;
+            }
+        }
+        break;
+
+    default:
+        errno = EAFNOSUPPORT;
+        return NULL;
+    } /* switch */
+
+    return dst;
+}
+#endif /* !HAVE_INET_NTOP */
+
+#if !HAVE_INET_PTON
+/* Fallback implementation of inet_pton() for systems that lack it,
+ * such as older versions of Windows (including MinGW builds that do
+ * not specifically target _WIN32_WINNT or newer).
+ *
+ * Based on code attributed to Paul Vixie, 1996,
+ * sourced from https://stackoverflow.com/a/15370175/4715872
+ */
+
+#define NS_INADDRSZ  sizeof(struct in_addr)  /*  4 */
+#define NS_IN6ADDRSZ sizeof(struct in6_addr) /* 16 */
+#define NS_INT16SZ   sizeof(uint16_t)        /*  2 */
+
+static int inet_pton4(const char *src, void *dst)
+{
+    uint8_t tmp[NS_INADDRSZ], *tp; /* for struct in_addr *dst */
+
+    int saw_digit = 0;
+    int octets = 0;
+    int ch;
+
+    *(tp = tmp) = 0;
+
+    while ((ch = *src++) != '\0') {
+        if (ch >= '0' && ch <= '9') {
+            uint32_t n = *tp * 10 + (ch - '0');
+
+            if (saw_digit && *tp == 0)
+                return 0;
+
+            if (n > 255)
+                return 0;
+
+            *tp = n;
+            if (!saw_digit) {
+                if (++octets > 4)
+                    return 0;
+                saw_digit = 1;
+            }
+        } else if (ch == '.' && saw_digit) {
+            if (octets == 4)
+                return 0;
+            *++tp = 0;
+            saw_digit = 0;
+        } else
+            return 0;
+    }
+    if (octets < 4)
+        return 0;
+
+    memcpy(dst, tmp, NS_INADDRSZ);
+
+    return 1;
+}
+
+static int inet_pton6(const char *src, void *dst)
+{
+    static const char xdigits[] = "0123456789abcdef";
+    uint8_t tmp[NS_IN6ADDRSZ]; /* for struct in6_addr *dst */
+
+    uint8_t *tp = (uint8_t *) memset(tmp, '\0', NS_IN6ADDRSZ);
+    uint8_t *endp = tp + NS_IN6ADDRSZ;
+    uint8_t *colonp = NULL;
+
+    const char *curtok = NULL;
+    int saw_xdigit = 0;
+    uint32_t val = 0;
+    int ch;
+
+    /* Leading :: requires some special handling. */
+    if (*src == ':') {
+        if (*++src != ':')
+            return 0;
+    }
+
+    curtok = src;
+
+    while ((ch = tolower(*src++)) != '\0') {
+        const char *pch = strchr(xdigits, ch);
+        if (pch != NULL) {
+            val <<= 4;
+            val |= (pch - xdigits);
+            if (val > 0xffff)
+                return 0;
+            saw_xdigit = 1;
+            continue;
+        }
+        if (ch == ':') {
+            curtok = src;
+            if (!saw_xdigit) {
+                if (colonp)
+                    return 0;
+                colonp = tp;
+                continue;
+            } else if (*src == '\0') {
+                return 0;
+            }
+            if (tp + NS_INT16SZ > endp)
+                return 0;
+            *tp++ = (uint8_t) (val >> 8) & 0xff;
+            *tp++ = (uint8_t) val & 0xff;
+            saw_xdigit = 0;
+            val = 0;
+            continue;
+        }
+        if (ch == '.' && ((tp + NS_INADDRSZ) <= endp) &&
+            inet_pton4(curtok, (char *) tp) > 0) {
+            tp += NS_INADDRSZ;
+            saw_xdigit = 0;
+            break; /* '\0' was seen by inet_pton4(). */
+        }
+        return 0;
+    }
+    if (saw_xdigit) {
+        if (tp + NS_INT16SZ > endp)
+            return 0;
+        *tp++ = (uint8_t) (val >> 8) & 0xff;
+        *tp++ = (uint8_t) val & 0xff;
+    }
+    if (colonp != NULL) {
+        /*
+         * Since some memmove()'s erroneously fail to handle
+         * overlapping regions, we'll do the shift by hand.
+         */
+        const int n = tp - colonp;
+        int i;
+
+        if (tp == endp)
+            return 0;
+
+        for (i = 1; i <= n; i++) {
+            endp[-i] = colonp[n - i];
+            colonp[n - i] = 0;
+        }
+        tp = endp;
+    }
+    if (tp != endp)
+        return 0;
+
+    memcpy(dst, tmp, NS_IN6ADDRSZ);
+
+    return 1;
+}
+
+static int inet_pton(int af, const char *src, void *dst)
+{
+    switch (af) {
+    case AF_INET:
+        return inet_pton4(src, dst);
+    case AF_INET6:
+        return inet_pton6(src, dst);
+    default:
+        return -1;
+    }
+}
+#endif /* !HAVE_INET_PTON */
+
 static int _modbus_tcp_init_win32(void)
 {
     /* Initialise Windows Socket API */
